236db7b2c0f9fc1b3d390012ff996258a6e91601
Ray.Fang <rayf.fang@mail.utorontoca>
2024-11-18 22:20:58 -0500

    Fixed SuggestedLocationsView

--- src/main/java/view/SuggestedLocationsView.java
+++ src/main/java/view/SuggestedLocationsView.java
@@ -12,29 +12,26 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import entity.Place;
-import interface_adapter.suggest_locations.SuggestLocationsController;
-import interface_adapter.suggest_locations.SuggestLocationsState;
-import interface_adapter.suggest_locations.SuggestLocationsViewModel;
+import interface_adapter.suggestlocation.SuggestedLocationsController;
+import interface_adapter.suggestlocation.SuggestedLocationsState;
+import interface_adapter.suggestlocation.SuggestedLocationsViewModel;
 
 /**
  * The View for when the user receives a list of suggested locations in the program.
  */
 public class SuggestedLocationsView extends JPanel implements ActionListener, PropertyChangeListener {
 
-    private final SuggestLocationsViewModel suggestLocationsViewModel;
-    private final SuggestLocationsController suggestLocationsController;
-    private final ViewManager viewManager;
+    private final SuggestedLocationsViewModel suggestedLocationsViewModel;
+    private final SuggestedLocationsController suggestedLocationsController;
 
     private final JPanel suggestedLocationsPanel;
     private final JButton newSearchButton;
 
-    public SuggestedLocationsView(SuggestLocationsViewModel suggestLocationsViewModel,
-                                  SuggestLocationsController suggestLocationsController,
-                                  ViewManager viewManager) {
-        this.suggestLocationsViewModel = suggestLocationsViewModel;
-        this.suggestLocationsViewModel.addPropertyChangeListener(this);
-        this.suggestLocationsController = suggestLocationsController;
-        this.viewManager = viewManager;
+    public SuggestedLocationsView(SuggestedLocationsViewModel suggestedLocationsViewModel,
+                                  SuggestedLocationsController suggestedLocationsController) {
+        this.suggestedLocationsViewModel = suggestedLocationsViewModel;
+        this.suggestedLocationsViewModel.addPropertyChangeListener(this);
+        this.suggestedLocationsController = suggestedLocationsController;
 
         final JLabel title = new JLabel("List of Suggested Locations:");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -50,28 +47,31 @@ public class SuggestedLocationsView extends JPanel implements ActionListener, Pr
         this.add(suggestedLocationsPanel);
         this.add(newSearchButton);
 
-        updateSuggestedLocations(suggestLocationsViewModel.getState());
+        updateSuggestedLocations(suggestedLocationsViewModel.getState());
     }
 
     @Override
     public void actionPerformed(ActionEvent evt) {
         if (evt.getSource().equals(newSearchButton)) {
-            viewManager.showLocationView();
+            // Handle new search action
+            System.out.println("New Search button clicked");
         }
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if ("state".equals(evt.getPropertyName())) {
-            updateSuggestedLocations((SuggestLocationsState) evt.getNewValue());
+            updateSuggestedLocations((SuggestedLocationsState) evt.getNewValue());
         }
     }
 
-    private void updateSuggestedLocations(SuggestLocationsState state) {
+    private void updateSuggestedLocations(SuggestedLocationsState state) {
         suggestedLocationsPanel.removeAll();
         for (Place location : state.getSuggestedLocations()) {
             suggestedLocationsPanel.add(new JLabel(location.getName()));
             suggestedLocationsPanel.add(new JLabel(location.getAddress()));
         }
+        suggestedLocationsPanel.revalidate();
+        suggestedLocationsPanel.repaint();
     }
 }
